deb1c530f33bf7afb0fabe996bc5b5272e4a42a0
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-13 15:31:36 +0000

    Added basic views and urls

--- eventhub/urls.py
+++ eventhub/urls.py
@@ -3,4 +3,8 @@ from django.conf.urls import url
 from eventhub import views
 from registration.backends.simple.views import RegistrationView
 
-urlpatterns = [ url(r'^$', views.index, name='index'), ]
+urlpatterns = [ url(r'^$', views.index, name='index'),
+                url(r'about/$', views.about, name='about'),
+                url(r'create_event$', views.create, name='create'),
+                url(r'recommended$', views.recommended, name='recommended'),
+            ]
--- eventhub/views.py
+++ eventhub/views.py
@@ -6,6 +6,15 @@ from registration.backends.simple.views import RegistrationView
 def index(request):
     return render(request,'eventhub/base.html')
 
+def about(request):
+    return render(request,'eventhub/about.html')
+
+def create(request):
+    return render(request,'eventhub/create_event.html')
+
+def recommended(request):
+    return render(request,'eventhub/recommended.html')
+
 
 #if successful at logging
 class MyRegistrationView(RegistrationView):
