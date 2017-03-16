b37bbf66ffc92cfe7c7b76cb5710c8d9c8fb1f37
Dominic Small <dominic.small@ntlworld.com>
2017-03-16 14:29:02 +0000

    Giving up on branches, here's the models for master

--- eventhub/models.py
+++ eventhub/models.py
@@ -3,3 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+class Event(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length = 128, unique = True)
+    creator = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return  self.user.username
