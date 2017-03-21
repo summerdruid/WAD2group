a523294898b0e3b0c706895575c495b2bf7b1840
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-21 16:37:02 +0000

    lil fix

--- eventhub/admin.py
+++ eventhub/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from eventhub.models import Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('category', 'title', 'loc', 'creator', 'date', 'time', 'id')
+    list_display = ('category', 'title', 'loc', 'creator', 'datetime', 'id', 'desc', 'pos')
 
 admin.site.register(Event, EventAdmin)
 
