7ac318cfcfb98e6b884a1e64d610000acd061c54
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-21 21:03:58 +0000

    major formatting and user profile

--- eventhub/models.py
+++ eventhub/models.py
@@ -3,23 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    preferences = models.CharField(max_length = 256, null = True, blank = True)
-
-    def __str__(self):
-        return self.user.username
-
-    def __unicode__(self):
-        return  self.user.username
-
 class Event(models.Model):
     category = models.CharField(max_length = 32)
     title = models.CharField(max_length = 256)
     loc = models.CharField(max_length = 128)
     pos = models.CharField(max_length = 128)
-    creator = models.ForeignKey(UserProfile)
+    creator = models.ForeignKey(User)
     datetime = models.DateTimeField()
     desc = models.CharField(max_length = 1024, null = True)
 
--- eventhub/views.py
+++ eventhub/views.py
@@ -1,41 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.decorators import login_required
 from eventhub.models import Event
 from django.db.models import Q
+
 
 def index(request):
     music = Event.objects.filter(category="music")[:3]
     business = Event.objects.filter(category="business")[:3]
     social = Event.objects.filter(category="social")[:3]
-    contdict = {'events': {'music': music, 'business': business, 'social': social}}
+    active = ["active","","","","","",""]
+    contdict = {'events': {'music': music, 'business': business, 'social': social}, 'active':active}
 
     return render(request,'eventhub/index.html', context=contdict)
 
 def about(request):
-    return render(request,'eventhub/about.html')
+    active = ["","active","","","","",""]
+    return render(request,'eventhub/about.html', context={'active':active})
 
 def create(request):
-    return render(request,'eventhub/create_event.html')
+    active = ["","","","active","","",""]
+    return render(request,'eventhub/create_event.html', context={'active':active})
 
 def event(request, eventID):
+    active = ["","","","","","",""]
     e = Event.objects.get(id=eventID)
-    return render(request, 'eventhub/event.html', context={'event': e})
-
-def recommended(request):
-    return render(request,'eventhub/recommended.html')
+    return render(request, 'eventhub/event.html', context={'event': e, 'active':active})
 
 def category(request, cat):
+    active = ["","","","","","",""]
     events = Event.objects.filter(category=cat)
     return render(request, 'eventhub/browse.html', context={'events': events, 'title': 'Category: '+cat})
 
 def search(request, search):
+    active = ["","","","","","",""]
     events = Event.objects.filter(Q(title__contains=search) | Q(loc__contains=search) | Q(category__contains=search))
     return render(request, 'eventhub/browse.html', context={'events': events, 'title': 'Looking for: '+search})
-    
+
 def profile(request):
-    currentuser = request.user
-    return(currentuser)
+    active = ["","","","","active","",""]
+    currentUser = request.user
+    events = Event.objects.filter(creator=currentUser)[:3]
+    return render(request, 'eventhub/profile.html', context={'active': active, 'name':currentUser, 'email':currentUser.email, 'events':events})
 
 #if successful at logging
 class MyRegistrationView(RegistrationView):
