c4fd73de2afe9107f894c4a699a9f395dcd876f2
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-24 14:32:47 +0000

    lf

--- WAD2group/settings.py
+++ WAD2group/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = ['michaelstarkey.pythonanywhere.com']
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 print(STATIC_DIR)
@@ -133,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = STATIC_DIR
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
