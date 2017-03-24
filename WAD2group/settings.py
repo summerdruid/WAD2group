0e4d1de7eea89d47f0929432941c22a3fbd8f3d8
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-24 13:09:14 +0000

    deletion and email notifications

--- WAD2group/settings.py
+++ WAD2group/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 '''added static n media dirs - by mary'''
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eventhubwadgroup@gmail.com'
+EMAIL_HOST_PASSWORD = 'eventhub'
 
 import os
 
